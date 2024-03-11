bfacfcfdd7cc2ce966707580fc33c89d300cc719
Lucas Hen <oisouhen@gmail.com>
2024-03-11 13:06:57 -0300

    Base

---
+++ Database/Database.sql
@@ -0,0 +1,140 @@
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts` (
+  `whitelist` tinyint(1) NOT NULL DEFAULT 0,
+  `chars` int(1) NOT NULL DEFAULT 1,
+  `gems` int(11) NOT NULL DEFAULT 0,
+  `premium` int(11) NOT NULL DEFAULT 0,
+  `predays` int(11) NOT NULL DEFAULT 0,
+  `priority` int(3) NOT NULL DEFAULT 0,
+  `login` varchar(25) NOT NULL DEFAULT '00/00/0000',
+  `discord` varchar(50) NOT NULL DEFAULT '0',
+  `steam` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`steam`) USING BTREE,
+  KEY `steam` (`steam`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `banneds`;
+CREATE TABLE `banneds` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `steam` varchar(100) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `days` int(3) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `characters`;
+CREATE TABLE `characters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `steam` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `serial` varchar(6) DEFAULT NULL,
+  `name` varchar(50) DEFAULT 'Individuo',
+  `name2` varchar(50) DEFAULT 'Indigente',
+  `bank` int(11) NOT NULL DEFAULT 1500,
+  `fines` int(11) NOT NULL DEFAULT 0,
+  `garage` int(3) NOT NULL DEFAULT 1,
+  `homes` int(3) NOT NULL DEFAULT 1,
+  `prison` int(11) NOT NULL DEFAULT 0,
+  `port` int(1) NOT NULL DEFAULT 0,
+  `penal` int(1) NOT NULL DEFAULT 0,
+  `deleted` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `chests`;
+CREATE TABLE `chests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `weight` int(10) NOT NULL DEFAULT 0,
+  `perm` varchar(100) NOT NULL,
+  `logs` int(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `entitydata`;
+CREATE TABLE `entitydata` (
+  `dkey` varchar(100) NOT NULL,
+  `dvalue` text DEFAULT NULL,
+  PRIMARY KEY (`dkey`),
+  KEY `dkey` (`dkey`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `playerdata`;
+CREATE TABLE `playerdata` (
+  `user_id` int(11) NOT NULL,
+  `dkey` varchar(100) NOT NULL,
+  `dvalue` text DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`dkey`),
+  KEY `user_id` (`user_id`),
+  KEY `dkey` (`dkey`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `prison`;
+CREATE TABLE `prison` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `police` varchar(255) DEFAULT '0',
+  `nuser_id` int(11) NOT NULL DEFAULT 0,
+  `services` int(11) NOT NULL DEFAULT 0,
+  `fines` int(11) NOT NULL DEFAULT 0,
+  `text` longtext DEFAULT NULL,
+  `date` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `propertys`;
+CREATE TABLE `propertys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL DEFAULT 'Homes0001',
+  `interior` varchar(255) NOT NULL DEFAULT 'Middle',
+  `tax` int(20) NOT NULL DEFAULT 0,
+  `user_id` int(11) NOT NULL DEFAULT 0,
+  `price` int(11) NOT NULL DEFAULT 0,
+  `residents` int(11) NOT NULL DEFAULT 1,
+  `vault` int(11) NOT NULL DEFAULT 1,
+  `fridge` int(11) NOT NULL DEFAULT 1,
+  `owner` int(1) NOT NULL DEFAULT 0,
+  `contract` int(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `races`;
+CREATE TABLE `races` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `raceid` int(11) NOT NULL DEFAULT 0,
+  `user_id` int(9) NOT NULL DEFAULT 0,
+  `name` varchar(255) DEFAULT NULL,
+  `vehicle` varchar(100) NOT NULL DEFAULT '0',
+  `points` int(20) NOT NULL DEFAULT 0,
+  `date` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `vehicles`;
+CREATE TABLE `vehicles` (
+  `user_id` int(11) NOT NULL,
+  `vehicle` varchar(100) NOT NULL,
+  `tax` int(20) NOT NULL DEFAULT 0,
+  `plate` varchar(20) DEFAULT NULL,
+  `hardness` int(1) NOT NULL DEFAULT 0,
+  `rental` int(11) NOT NULL DEFAULT 0,
+  `rendays` int(11) NOT NULL DEFAULT 0,
+  `arrest` int(11) NOT NULL DEFAULT 0,
+  `time` int(11) NOT NULL DEFAULT 0,
+  `engine` int(11) NOT NULL DEFAULT 1000,
+  `body` int(11) NOT NULL DEFAULT 1000,
+  `fuel` int(11) NOT NULL DEFAULT 100,
+  `work` varchar(10) NOT NULL DEFAULT 'false',
+  `doors` varchar(254) NOT NULL,
+  `windows` varchar(254) NOT NULL,
+  `tyres` varchar(254) NOT NULL,
+  `brakes` varchar(254) NOT NULL,
+  PRIMARY KEY (`user_id`,`vehicle`),
+  KEY `user_id` (`user_id`),
+  KEY `vehicle` (`vehicle`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
